04f451d76863506aa092c0c80898c06c6a81a691
banzaben892 <banzaben892@gmail.com>
2026-06-03 13:51:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 function sendMessage() {
   let input = document.getElementById("input");
   let msg = input.value.trim();
-
   if (!msg) return;
 
   addMessage(msg, "user");
+  input.value = "";
+
+  showTyping();
 
   let analysis = IA1(msg);
   let reply = IA2(analysis, msg);
 
   setTimeout(() => {
-    addMessage(reply, "bot");
-  }, 400);
-
-  input.value = "";
+    removeTyping();
+    typeMessage(reply);
+  }, 900);
 }
 
+/* ADD MESSAGE */
 function addMessage(text, type) {
   let box = document.getElementById("chatBox");
 
@@ -24,6 +26,52 @@ function addMessage(text, type) {
   div.innerText = text;
 
   box.appendChild(div);
+  box.scrollTop = box.scrollHeight;
+}
+
+/* TYPING INDICATOR */
+function showTyping() {
+  let box = document.getElementById("chatBox");
+
+  let div = document.createElement("div");
+  div.classList.add("msg", "bot");
+  div.id = "typing";
+  div.innerHTML = `
+    <div class="typing">
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+    </div>
+  `;
+
+  box.appendChild(div);
+  box.scrollTop = box.scrollHeight;
+}
+
+function removeTyping() {
+  let t = document.getElementById("typing");
+  if (t) t.remove();
+}
+
+/* TYPEWRITER EFFECT */
+function typeMessage(text) {
+  let box = document.getElementById("chatBox");
+
+  let div = document.createElement("div");
+  div.classList.add("msg", "bot");
+  box.appendChild(div);
+
+  let i = 0;
+
+  function typing() {
+    if (i < text.length) {
+      div.innerText += text.charAt(i);
+      i++;
+      setTimeout(typing, 15);
+    }
+  }
+
+  typing();
 
   box.scrollTop = box.scrollHeight;
-    }
+}
